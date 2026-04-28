034f8f9eb506aebe7bcda7a9f2575d9ec942e297
Mary <mhb93@nau.edu>
2026-04-28 13:42:19 -0800

    Mutable vs immutable variables. Turns out Rust reeeeeeeeeeeeeeeeeeeeeally like snake case things (I like camel case more).

---
+++ mutable_vars_and_printing.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let immutableImogen = 14;
+    println!("The variable immutableImogen, whose value is {immutableImogen}, is immutable.");
+    let mut mutableMandy = 999;
+    println!("The variable mutableMandy, whose value is {mutableMandy}, is mutable.");
+    println!("Now I will change mutableMandy.");
+    mutableMandy = 348;
+    println!("mutableMandy's value is {mutableMandy}");
+}
