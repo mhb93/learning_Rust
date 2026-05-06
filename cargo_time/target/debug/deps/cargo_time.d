4dfe61075a209c214f60c25617fb7a0a233b8d3a
Mary Bolling <71998729+mhb93@users.noreply.github.com>
2026-05-05 22:01:08 -0800

    Delete cargo_time/target directory

--- cargo_time/target/debug/deps/cargo_time.d
+++
@@ -1,5 +0,0 @@
-C:\Users\wisea\OneDrive\Desktop\Code stuff\learning_Rust\cargo_time\target\debug\deps\cargo_time.d: src\main.rs
-
-C:\Users\wisea\OneDrive\Desktop\Code stuff\learning_Rust\cargo_time\target\debug\deps\cargo_time.exe: src\main.rs
-
-src\main.rs:
