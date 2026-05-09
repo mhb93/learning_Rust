881fc6588894cdb4418e16eeb92d4ed748df8f1f
Mary <mhb93@nau.edu>
2026-05-08 16:52:58 -0800

    Created the main body of a program similar to SNL's Guess That Dog! skit.

---
+++ guess_that_dog/src/main.rs
@@ -0,0 +1,42 @@
+// This program is based on the Saturday Night Live sketch Name That Dog!
+// The idea is to guess the name of the dog by looking at it.
+// It's still a work in progress...
+// Last updated 05/08/2026
+
+// The names of the dogs that the user will be shown throughout the game
+const DOGNAMES: [&str; 3] = ["Cooper", "Lobo", "Shep"];
+
+#[allow(non_snake_case)] // I am a camelCase stan
+fn main() {
+    // Introduction
+    println!("Welcome to Name That Dog!");
+    println!("(This game was based on the Saturday Night Live sketch of the same name. It's pretty funny and I think you should look it up.)");
+    println!("Now, take a look at this dog: ");
+
+    // Dogs
+    let array: [String; 3] = ["  __    __\no-''))_____\\\n\"--__/ * * * )\nc_c__/-c____/".to_string(),
+                              "         __\n        /  \\\n       / ..|\\\n      (_\\  |_)\n      /  \\@' \n     /     \\\n_   /  `   |\n\\\\/  \\  | _\\\n \\   /_ || \\\\_\n  \\____)|_) \\_)".to_string(),
+                              "             ____\n            /    \\__\n|\\         /    @   \\\n\\ \\_______|    \\   .:|>\n \\      ##|    | \\__/\n  |    ####\\__/   \\\n  /  /  ##       \\|\n /  /__________\\  \\\n L_JJ           \\__JJ".to_string(),
+                              ];
+
+    // TODO: randomize which dog gets shown
+    // TODO: put this all in a loop for ENDLESS FUN OH BOY
+    let arrVar = &array[2];
+    println!("{arrVar}");
+    println!("Now, name that dog!\n(Type in the dog's name and then press enter.)");
+    // Choose the dog's name
+    let dogNameNum: usize = rand::random_range(0..(DOGNAMES.len() - 1));
+    let mut dogName = DOGNAMES[dogNameNum].to_string();
+
+    // Take a guess from the user
+    let mut guess = String::new();
+    std::io::stdin().read_line(&mut guess).expect("Oopsie, I didn't get that.");
+    guess = guess.trim().to_string();
+
+    // Important: user cannot get it right, ever
+    if dogName == guess {
+        dogName = (DOGNAMES[dogNameNum + 1]).to_string();
+    }
+    println!("Oh, I'm so sorry, but that's incorrect!");
+    println!("This dog's name is {}.", dogName);
+}
